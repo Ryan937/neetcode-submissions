aade0732b438b9e8cf9de8b90b6f6e2318c63465
Ryan937 <lsj937@gmail.com>
2026-06-19 23:47:04 -0700

    Add: count-prefix-and-suffix-pairs-i - submission-4

---
+++ Data Structures & Algorithms/count-prefix-and-suffix-pairs-i/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countPrefixSuffixPairs(self, words: List[str]) -> int:
+        def isPrefixAndSuffix(str1: str, str2: str) -> bool:
+            return str2.startswith(str1) and str2.endswith(str1)
+
+        count = 0
+
+        for i in range(len(words)):
+            for j in range(i + 1, len(words)):
+                if isPrefixAndSuffix(words[i], words[j]):
+                    count += 1
+
+        return count
